e420e25c595ba2c214d43f0636b160eb08b5a6e9
Muhammad Fathurrohim <m.faturrohim13@gmail.com>
2025-11-07 09:07:01 +0800

    feat: change zsh to fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,32 @@
+
+if status is-interactive
+    # set -gx MCFLY_LIGHT TRUE
+
+    zoxide init fish | source
+    if test -z $fish_private_mode 
+      mcfly init fish | source
+    end
+    direnv hook fish | source
+    function __onpwd --on-variable PWD
+        set --universal LAST_WORKING_DIR $PWD
+    end
+end
+
+fenv source ~/.profile
+
+function cdl --wraps='cd' --description 'go to last working directory'
+    cd "$LAST_WORKING_DIR"
+end
+
+function rmdirr --wraps='find' --description 'remove directory recursively'
+    find -D all $argv -type d -empty -delete
+end
+
+function dlv --wraps='yt-dlp'
+  yt-dlp -S "res:480" $argv
+end
+
+# function sway
+#   /bin/sway $argv >> ~/.local/state/sway.log 2>&1
+# end
+# pnpm end
