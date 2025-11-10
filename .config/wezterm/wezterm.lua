7098b72857b2a0817356b3dcf7f9fb6dec3c6d39
Muhammad Fathurrohim <m.faturrohim13@gmail.com>
2025-11-11 05:15:39 +0800

    feat: add wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,16 @@
+local wezterm = require("wezterm")
+
+local config = {}
+
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+local colors, _ = wezterm.color.load_scheme(wezterm.config_dir .. "/github-dark-default.toml")
+config.colors = colors
+config.window_background_opacity = 0.9
+config.enable_tab_bar = false
+
+config.font_size = 11
+
+return config
