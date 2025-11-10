ff9a82c639670e32d953b808439a6c3174c0b4b4
Muhammad Fathurrohim <m.faturrohim13@gmail.com>
2025-11-11 06:24:49 +0800

    use vi mode in fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,15 +1,16 @@
-
 if status is-interactive
     # set -gx MCFLY_LIGHT TRUE
 
     zoxide init fish | source
-    if test -z $fish_private_mode 
-      mcfly init fish | source
+    if test -z $fish_private_mode
+        mcfly init fish | source
     end
     direnv hook fish | source
     function __onpwd --on-variable PWD
         set --universal LAST_WORKING_DIR $PWD
     end
+
+    fish_vi_key_bindings
 end
 
 fenv source ~/.profile
@@ -23,7 +24,7 @@ function rmdirr --wraps='find' --description 'remove directory recursively'
 end
 
 function dlv --wraps='yt-dlp'
-  yt-dlp -S "res:480" $argv
+    yt-dlp -S "res:480" $argv
 end
 
 # function sway
