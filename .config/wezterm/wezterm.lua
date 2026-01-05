4101e507b201c272d10c69f1b347749a240772cc
Muhammad Fathurrohim <m.faturrohim13@gmail.com>
2026-01-05 15:01:22 +0800

    fix(wezterm): WLR_RENDERER vulkan changes opacity

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,11 +7,25 @@ if wezterm.config_builder then
 end
 
 local colors, _ = wezterm.color.load_scheme(wezterm.config_dir .. "/github-dark-default.toml")
+local wlr_renderer = os.getenv("WLR_RENDERER")
+
+local is_wlr_vulkan = false
+if wlr_renderer ~= nil then
+	local is_wlr_vulkan = string.find(wlr_renderer, "vulkan")
+end
+
+config.front_end = "WebGpu"
 config.colors = colors
-config.window_background_opacity = 0.9
+
+if is_wlr_vulkan then
+	config.window_background_opacity = 0.97
+else
+	config.window_background_opacity = 0.9
+end
+
 config.enable_tab_bar = false
 
-config.font = wezterm.font 'FiraCode Nerd Font'
+config.font = wezterm.font("FiraCode Nerd Font")
 config.font_size = 11
 
 return config
